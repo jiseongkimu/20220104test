9d5c0dba37aeb55b1edd097428909920256d3782
jiseongkimu <jiseongkimu@gmail.com>
2022-01-06 19:13:38 +0900

    docs : 리스트뷰 추가

--- app/src/main/java/org/techtown/a20220104test/MainActivity.java
+++ app/src/main/java/org/techtown/a20220104test/MainActivity.java
@@ -3,6 +3,10 @@ package org.techtown.a20220104test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+/*
+        ArrayAdapter adapter = new ArrayAdapter(this, R.id.simple_list_item, LIST_MENU);
+
+        ListView listview = (ListView) findViewById(R.id.simple_list);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+
+                // get TextView's Text.
+                String strText = (String) parent.getItemAtPosition(position) ;
+
+                // TODO : use strText
+            }
+        }) ;
+*/
+
+
     }
+
+
+
+    static final String[] LIST_MENU = {"ID", "CONTENTS","TIME"};
 }
