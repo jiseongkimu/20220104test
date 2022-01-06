1328c2cd63f58514a57b4bd7c5bbc74e0f54734c
jiseongkimu <jiseongkimu@gmail.com>
2022-01-06 19:34:53 +0900

    docs : 주석 추가

--- app/src/main/java/org/techtown/a20220104test/MainActivity.java
+++ app/src/main/java/org/techtown/a20220104test/MainActivity.java
@@ -3,9 +3,11 @@ package org.techtown.a20220104test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,31 +15,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*
+        * 해당 메소드의 인자로 레이아웃 리소스 ID를 전달(객체와-인플레이션)
+        * 실행하게 되면 activity_main.xml 로 만든 레이아웃이 출력됨(뷰의 설정 배치)
+        * 뒤에 findViewById()와 같은 메서드가 실행되기 위해 필수적임
+        */
         setContentView(R.layout.activity_main);
 
-/*
-        ArrayAdapter adapter = new ArrayAdapter(this, R.id.simple_list_item, LIST_MENU);
-
-        ListView listview = (ListView) findViewById(R.id.simple_list);
-        listview.setAdapter(adapter);
-
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
+        /*
+        * findViewById() : ID 값을 이용해 특정 뷰를 받아오는 메서드
+        * */
+        Button button = (Button) findViewById(R.id.buy);
+        button.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onItemClick(AdapterView parent, View v, int position, long id) {
-
-                // get TextView's Text.
-                String strText = (String) parent.getItemAtPosition(position) ;
-
-                // TODO : use strText
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
             }
-        }) ;
-*/
-
+        });
 
     }
-
-
-
-    static final String[] LIST_MENU = {"ID", "CONTENTS","TIME"};
 }
